56f2029d20f7238ed9a7a9d03871b05eb82cac2c
Navdeep K <kantenavdeep@outlook.com>
2021-06-13 18:08:59 +0530

    fixed all bugs, added clear button, remove strict

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-"use strict"
-
 function convert(original) {
 
     let originalArray = original.split(' ');
@@ -18,7 +16,8 @@ function convert(original) {
             }
             // Compose final wording
             // let convertedCons = originalWord.slice(vowelIndex + originalWord.slice(0, vowelIndex)) + "ay";
-            let convertedCons = originalWord.slice(0, vowelIndex) + "ay";
+            // let convertedCons = originalWord.slice(0, vowelIndex) + "ay";
+            let convertedCons = originalWord.slice(vowelIndex) + originalWord.slice(0, vowelIndex) + "ay"
             convertedArray.push(convertedCons);
         }
     }
@@ -32,9 +31,15 @@ function convert(original) {
 // Get it from DOM
 
 let inputButton = document.getElementById('input-btn');
-
 inputButton.addEventListener('click', function() {
     let originalText = document.getElementById('input-text');
     let pigLatinPara = document.getElementById('output-text');
     pigLatinPara.innerText = convert(originalText.value);
+});
+
+
+let clearButton = document.getElementById('clear-btn');
+clearButton.addEventListener('click', function() {
+    let para = document.getElementById('output-text');
+    para.innerText = `Here Comes Nothing`;
 });
